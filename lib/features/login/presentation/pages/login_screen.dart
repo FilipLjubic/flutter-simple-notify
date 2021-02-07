fa53d72bdf5acb8f941e7b4b369ec37c7937d9f7
FilipLjubic <filip.ljubic@fer.hr>
2021-02-07 12:37:25 +0100

    init

---
+++ lib/features/login/presentation/pages/login_screen.dart
@@ -0,0 +1,83 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Icon(Icons.message_outlined, size: screenHeight * 0.15),
+          SizedBox(height: 10.0),
+          Text(
+            "Sign in",
+            style: TextStyle(fontSize: 24.0),
+          ),
+          SizedBox(height: 10.0),
+          SignInContainer(
+            screenWidth: screenWidth,
+            icon: Icon(Icons.account_circle_rounded),
+            text: Text("Continue with Google"),
+          ),
+          SizedBox(height: 10.0),
+          SignInContainer(
+            screenWidth: screenWidth,
+            icon: Icon(Icons.account_circle_rounded),
+            text: Text("Continue with Facebook"),
+          ),
+          SizedBox(height: 20.0),
+          Text("or"),
+          SizedBox(height: 20.0),
+          SignInContainer(
+            screenWidth: screenWidth,
+            text: Text("Skip signing in"),
+            icon: Icon(Icons.assignment_ind_rounded),
+            color: Colors.red[100],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class SignInContainer extends StatelessWidget {
+  const SignInContainer({
+    Key key,
+    @required this.screenWidth,
+    @required this.text,
+    @required this.icon,
+    this.color,
+  }) : super(key: key);
+
+  final double screenWidth;
+  final Icon icon;
+  final Text text;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.1),
+      child: Container(
+        width: screenWidth * 0.8,
+        height: 50,
+        decoration: BoxDecoration(
+          color: color != null ? color : Colors.blueGrey[100],
+          borderRadius: BorderRadius.circular(5),
+        ),
+        child: ListTile(
+          minVerticalPadding: 0.0,
+          leading: icon,
+          title: Center(child: text),
+          isThreeLine: false,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:equatable/equatable.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:notify/features/login/presentation/pages/login_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  EquatableConfig.stringify = kDebugMode;
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Simple Notify',
+      home: LoginScreen(),
+    );
+  }
+}
